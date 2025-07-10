5778b3d4a2c9a1662c6aa3c7fb728c4afe1c9b68
Rayyan <rayhq3@gmail.com>
2025-07-10 14:04:54 +0530

    learned abt css modules

--- fragments/src/App.jsx
+++ fragments/src/App.jsx
@@ -1,12 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import FoodItem from "./components/FoodItems";
 import ErrorMessage from "./components/ErrorMessage";
+import "./App.css";
 
 function App() {
   let foodItems = ["dal", "Green Vegies", "Roti", "Mutton", "CHicken"];
   return (
     <>
-      <div>Healthy Foods</div>
+      <h1 className="food-heading">Healthy Foods</h1>
       <ErrorMessage items={foodItems}></ErrorMessage>
       <FoodItem items={foodItems}></FoodItem>
     </>
--- fragments/src/components/Item.jsx
+++ fragments/src/components/Item.jsx
@@ -1,4 +1,10 @@
+import styles from "./Item.module.css";
+
 function Item({ foodItems }) {
-  return <li className="list-group-item">{foodItems}</li>;
+  return (
+    <li className={`${styles["kg-item"]} list-group-item`}>
+      <span className={styles["kg - span"]}>{foodItems}</span>
+    </li>
+  );
 }
 export default Item;
