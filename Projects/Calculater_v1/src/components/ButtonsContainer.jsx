0b72b187f36bcf1dfb073773644c5699798cecdb
Rayyan <rayhq3@gmail.com>
2025-07-11 12:04:13 +0530

    rendered buttons using map method

--- Projects/Calculater_v1/src/components/ButtonsContainer.jsx
+++ Projects/Calculater_v1/src/components/ButtonsContainer.jsx
@@ -1,13 +1,30 @@
 import styles from "./ButtonsContainer.module.css";
 function ButtonsContainer() {
-  let buttonNames = ["c", "1", "2", "+"];
+  let buttonNames = [
+    "c",
+    "1",
+    "2",
+    "+",
+    "3",
+    "4",
+    "-",
+    "5",
+    "6",
+    "*",
+    "7",
+    "8",
+    "/",
+    "=",
+    "9",
+    "0",
+    ".",
+  ];
   return (
     <>
       <div className={styles.buttonsContainer}>
-        <button className={styles.button}>C</button>
-        <button className={styles.button}>1</button>
-        <button className={styles.button}>2</button>
-        <button className={styles.button}>+</button>
+        {buttonNames.map((buttonName) => (
+          <button className={styles.button}>{buttonName}</button>
+        ))}
       </div>
     </>
   );
