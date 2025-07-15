fc268d7726dcb1ab7b598be1d2c8905e945bebab
Rayyan <rayhq3@gmail.com>
2025-07-15 15:44:58 +0530

    working on calculater_v2

---
+++ Projects/Calculater_v2/src/components/Display.jsx
@@ -0,0 +1,9 @@
+import styles from "./Display.module.css";
+function Display() {
+  return (
+    <>
+      <input className={styles.display} type="text" />
+    </>
+  );
+}
+export default Display;
