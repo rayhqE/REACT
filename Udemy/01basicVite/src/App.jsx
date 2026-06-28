1e086936cfdc5d4b84ac6760759d9479e33a267e
Rayyan <rayhq3@gmail.com>
2026-06-28 16:08:31 +0530

    custom react nd JSX

--- Udemy/01basicVite/src/App.jsx
+++ Udemy/01basicVite/src/App.jsx
@@ -1,8 +1,9 @@
 import Youtube from "./Youtube";
 function App() {
+  const username = "rayhq3";
   return (
     <>
-      <h1>Vite React app</h1>
+      <h1>Vite React app {username}</h1>
       <h1>Vite React app</h1>
       <Youtube></Youtube>
     </>
